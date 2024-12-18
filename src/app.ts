27e2ab01883566f513aafb872593e2505afdc338
Mass-Nagaraj <rnagaraj6699@gmail.com>
2024-12-18 09:25:27 +0530

    added first

--- src/app.ts
+++ src/app.ts
@@ -33,13 +33,13 @@ declare global {
 
 
 global.status_codes = {
-  success: { status: 200, api_status: "API-OK" },
-  success_no_data: { status: 200, api_status: "API-OK-NO-CONTENT" },
-  bad_request: { status: 400, api_status: "API-BAD-REQUEST" },
-  un_authorised: { status: 401, api_status: "API-UN-AUTHORISED-ACCESS" },
-  forbidden: { status: 403, api_status: "API-FORBIDDEN" },
-  not_found: { status: 404, api_status: "API-NOT-FOUND" },
-  error: { status: 500, api_status: "API-ERROR" },
+    success: { status: 200, api_status: "API-OK" },
+    success_no_data: { status: 200, api_status: "API-OK-NO-CONTENT" },
+    bad_request: { status: 400, api_status: "API-BAD-REQUEST" },
+    un_authorised: { status: 401, api_status: "API-UN-AUTHORISED-ACCESS" },
+    forbidden: { status: 403, api_status: "API-FORBIDDEN" },
+    not_found: { status: 404, api_status: "API-NOT-FOUND" },
+    error: { status: 500, api_status: "API-ERROR" },
 };
 
 
