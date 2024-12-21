dc5b57dfad23608713c07df6d91f2932982e0d24
Mass-Nagaraj <rnagaraj6699@gmail.com>
2024-12-21 17:47:00 +0530

    add

--- src/app.ts
+++ src/app.ts
@@ -48,14 +48,14 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-    void  fastify.register(cors, {
-          origin: true,
-          methods: ['GET', 'POST', 'DELETE', 'PATCH',"OPTIONS"],
-          credentials: true,
-          allowedHeaders: ['Content-Type', 'Authorization'] ,
-          preflightContinue:true,
-          preflight:true
-        });
+    // void  fastify.register(cors, {
+    //       origin: true,
+    //       methods: ['GET', 'POST', 'DELETE', 'PATCH',"OPTIONS"],
+    //       credentials: true,
+    //       allowedHeaders: ['Content-Type', 'Authorization'] ,
+    //       preflightContinue:true,
+    //       preflight:true
+    //     });
 
   void fastify.register(AutoLoad, {
    
