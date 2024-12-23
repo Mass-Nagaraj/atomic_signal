e84730c2e022af6dfdfb693023bc9bf46b7513bc
Mass-Nagaraj <rnagaraj6699@gmail.com>
2024-12-23 16:48:38 +0530

    gf

--- src/controllers/Grades/index.ts
+++ src/controllers/Grades/index.ts
@@ -1,35 +1,36 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import { addSignal, UpdateSignalSchema } from "../../routes/v1/private/signals/schema";
+import {
+  addSignal,
+  UpdateSignalSchema,
+} from "../../routes/v1/private/signals/schema";
 import { prisma } from "../../utils.ts/prisma";
 import { UpdateGrade } from "../../routes/v1/private/Grading/schema";
 
 interface GradeId {
-    grade_id:string,
+  grade_id: string;
 }
 
-
 export async function edit_grading(
-    request:FastifyRequest<{
-         Params: GradeId,
-         Body:UpdateGrade 
-    }>,
-    reply:FastifyReply
+  request: FastifyRequest<{
+    Params: GradeId;
+    Body: UpdateGrade;
+  }>,
+  reply: FastifyReply
 ) {
-    try{
+  try {
+    const { name } = request.body;
+    const { grade_id } = request.params; // This should be {} closed bracktes .Params varcables
+    const updated_grade = await prisma.grading_master.update({
+      //
+      //
+      where: {
+        id: grade_id,
+      },
+      data: { name },
+    });
 
-        const {name}=request.body;
-        const {grade_id} = request.params; // This should be {} closed bracktes .Params varcables
-        const updated_grade = await prisma.grading_master.update({
-            where: {
-                id: grade_id,
-            },
-            data: {name},
-         
-        });
-        
-        return reply.code(200).send(updated_grade);
-
-    }catch(error) {
-        return reply.code(200).send(error)
-    }
+    return reply.code(200).send(updated_grade);
+  } catch (error) {
+    return reply.code(200).send(error);
+  }
 }
